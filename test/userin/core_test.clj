c69f9c401b25fb155a9f525ab7f20e03357a66e1
Fabio José <fabiojose@gmail.com>
2019-11-08 12:33:46 -0300

    Testing the json-parse function
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- test/userin/core_test.clj
+++ test/userin/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [userin.core :refer :all]))
 
+(deftest test-json-parse
+  (testing "Should return a parsed json"
+    (is (= {:name "clojure"} (json-parse "{\"name\" : \"clojure\"}"))))
+  (testing "Should throw exception on invalid json"
+    (is (thrown? Exception (json-parse "llç")))))
+
 (deftest test-is-a-text
   (testing "Should return false when text is not valid"
     (is (= (is-a-text "") false))
