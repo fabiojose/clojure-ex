34a039e71e68b01536f039a992397191ca9eb256
Fabio José <fabiojose@gmail.com>
2019-11-08 10:15:19 -0300

    Improvement of stdreading, import json parser
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- src/userin/core.clj
+++ src/userin/core.clj
@@ -1,9 +1,13 @@
 (ns userin.core
-  (:gen-class))
+  (:gen-class)
+  (:require [cheshire.core :refer :all]))
+
+(defn parse [value]
+  (parse-string value true))
 
 (defn read-all []
   (loop [line (read-line)]
-    (when (not (= line ""))
+    (when (and (not (= line "")) (not (= line nil)))
       (println line)
       (recur (read-line)))))
 
