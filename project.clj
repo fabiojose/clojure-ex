6298bfb382044365cfd210177a3908cb46569126
Fabio José <fabiojose@gmail.com>
2019-11-08 10:14:50 -0300

    Dependency for json parsing
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject userin "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :description "App to read stdin"
+  :url "https://github.com/fabiojose/clojure-ex"
+  :license {:name "Apache-2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [cheshire "5.9.0"]]
   :main userin.core
   :profiles {:uberjar {:aot :all}}
   :uberjar-name "app.jar")
