028422fe0a6894aa566b0973df0249a5480b124c
Fabio José <fabiojose@gmail.com>
2019-11-08 11:07:56 -0300

    Dependency for coverage computation
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/fabiojose/clojure-ex"
   :license {:name "Apache-2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [cheshire "5.9.0"]]
   :main userin.core
