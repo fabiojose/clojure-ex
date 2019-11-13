800ebd71d7e35f81d09410e34bdd67d9ccb4d99a
Fabio José <fabiojose@gmail.com>
2019-11-13 17:06:07 -0300

    :alembic: Processing the activeCard
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- src/userin/core.clj
+++ src/userin/core.clj
@@ -10,11 +10,29 @@
 (defn is-a-text [text]
   (and (not (= text "")) (not (= text nil))))
 
-;; to decide what to do
-(defn decide [json]
+;; to check if a given json is an account object
+(defn is-account [json]
+  (contains? json :account))
+
+;; to check if a given account has an active card
+(defn is-card-active [account]
+  (if (get-in account [:account :activeCard])
+    account
+    (assoc account :violations ["card-not-active"])))
+
+;; to check if a given account has limit to process the tx amount
+; (defn has-limit [tx account]
+;   (if (get-in account [:account :availableLimit])))
+
+;; to check if a given tx should be authorized over an account
+(defn authorize [tx account]
+  (is-card-active account))
+
+;; to decide what gonna do with parsed json
+(defn decide [json account]
   (cond
-    (contains? json :account) (println "account")
-    (contains? json :transaction) (println "transaction")
+    (contains? json :account) json
+    (contains? json :transaction) (authorize json account)
     :else (throw (Exception. (str "unsupported json: " json)))))
 
 ;; to read stdin until end
@@ -24,7 +42,19 @@
       (decide (json-parse line))
       (recur (read-line)))))
 
+(defn read-all2 []
+  (loop [line (read-line) account nil]
+    (when (is-a-text line)
+      (println (json-parse line) account)
+      (recur (read-line) (if (is-account (json-parse line)) (json-parse line) account) ))))
+
+(defn read-all3 []
+  (loop [line (read-line) account nil]
+    (when (is-a-text line)
+      (println line account)
+      (recur (read-line) (decide (json-parse line) account)))))
+
 ;; the main function
 (defn -main
   [& args]
-  (read-all))
+  (read-all3))
