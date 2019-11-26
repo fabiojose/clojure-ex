7f6d8561395daf529dc8b378e0fd78a510091c78
Fabio José <fabiojose@gmail.com>
2019-11-26 17:09:17 -0300

    :heavy_plus_sign: new dep for date-time processing
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [cheshire "5.9.0"]]
+                 [cheshire "5.9.0"]
+                 [clojure.java-time "0.3.2"]]
   :main userin.core
   :profiles {:uberjar {:aot :all}}
   :uberjar-name "app.jar")
